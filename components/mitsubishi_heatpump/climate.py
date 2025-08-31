eb07ad3c718cc1018357469a641590f5453a299e
Luckyp <16467073+luckyp70@users.noreply.github.com>
2025-08-31 12:09:48 +0200

    Update CONFIG_SCHEMA to include MitsubishiHeatPump

--- components/mitsubishi_heatpump/climate.py
+++ components/mitsubishi_heatpump/climate.py
@@ -63,7 +63,7 @@ SELECT_SCHEMA = select.SELECT_SCHEMA.extend(
     {cv.GenerateID(CONF_ID): cv.declare_id(MitsubishiACSelect)}
 )
 
-CONFIG_SCHEMA = climate.climate_schema().extend(
+CONFIG_SCHEMA = climate.climate_schema(MitsubishiHeatPump).extend(
     {
         cv.GenerateID(): cv.declare_id(MitsubishiHeatPump),
         cv.Optional(CONF_HARDWARE_UART, default="UART0"): valid_uart,
